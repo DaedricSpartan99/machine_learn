faa3a07b4431d74ed89e3b6da2121f46e6947199
DaedricSpartan99 <leleraffa97@hotmail.it>
2020-10-25 09:38:00 +0100

    Added threshold

--- P1/scripts/implementations.py
+++ P1/scripts/implementations.py
@@ -1,13 +1,18 @@
 import numpy as np
 import random as rnd
 
+# define threshold constant
+eps = 1e-5
+
 def mini_batch_SDG(y, tx, grad_n, initial_w, max_iters, gamma):
 
     N = len(y) # length of samples
     Nx = len(tx[0]) # length of arguments
-    w = initial_w
+    w = initial_w + np.ones(Nx)
+    old_w = initial_w
 
-    while max_iters > 0: # TODO add threshold condition
+    # iterate
+    while max_iters > 0 and (np.norm(w - old_w) / Nx) > eps: # TODO add threshold condition
 
         # generate random integer to consider
         NB = rnd.randint(1,N)
@@ -19,8 +24,12 @@ def mini_batch_SDG(y, tx, grad_n, initial_w, max_iters, gamma):
             g += grad_n(grad_n(y[n], tx[n]))
         g /= NB
 
+        old_w = w
+
         # forward step w
         w -= gamma * g
+
+        # decrement step
         max_iters -= 1
 
     return w
@@ -52,4 +61,17 @@ def least_squares(y, tx):
     # Compute weight as particular case of ridge regression, _lambda = 0
     return ridge_regression(y, tx, 0)
 
+def logistic_sigma(z):
+    arg = np.exp(z) 
+    return arg / (1 + arg)
+
+def logistic_regression(y, tx, initial_w, max_iters, gamma):
+
+    # gradient L_n formula: x_n * (sigma(x_n * w) - y_n)
+    grad_n = lambda yn, txn, w: txn * (logistic_sigma(np.dot(ntx, w)) - yn)
+
+    # compute optimal weight
+    w = mini_batch_SDG(y, tx, grad_n, initial_w, max_iters, gamma)
+
+    return w, compute_RMSE(y, tx, w)
 
